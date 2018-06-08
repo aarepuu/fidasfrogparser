b891790859dc992a470f074079058e0aa5e3ed1e
Aare Puussaar <a.puussaar2@ncl.ac.uk>
2018-06-08 16:54:37 -0700

    Script updates GPS head
    Added gps header to command line command

--- fidas-parser.py
+++ fidas-parser.py
@@ -1,4 +1,7 @@
-#!/usr/bin/env python
+#remove millis
+    gps[gheader] = gps[gheader].values.astype('datetime64[s]')
+    #remove dublicates
+    gps = gps.drop_duplicates(gheader,keep='last')#!/usr/bin/env python
 
 # -*- coding: utf-8 -*-
 """Fidas Frog dataparser.
@@ -16,6 +19,7 @@ Todo:
     * Add comment extraction function
     * Add formating arguments
     * Add custom headers
+    * Add fidas frog v2 support
 
 
   Copyright (c) 2017, Open Lab Newcastle University, UK. 
@@ -50,7 +54,7 @@ __copyright__ = "Copyright (c) 2017, Newcastle University, UK."
 __license__ = "MIT"
 __maintainer__ = "Aare Puussaar"
 __email__ = "a.puussaar2@ncl.ac.uk"
-__version__ = "0.4"
+__version__ = "0.51"
 __status__ = "Development"
 
 
@@ -59,7 +63,7 @@ import pandas as pd
 from datetime import datetime, timedelta
 
 
-def addGPS(readings,gps,gheader):
+def addGPS(readings,gps,gheader,tformat):
     """Function for getting nearest timestamp gps location
 
         TODO - needs testing
@@ -78,10 +82,13 @@ def addGPS(readings,gps,gheader):
     
     #if readings are in epoch
     #Timezone issues with the parser
-    readings.timestamp = readings.timestamp.astype("datetime64[s]") - timedelta(hours=1)
-    #TODO - add format as argument
-    gps[gheader] = pd.to_datetime(gps[gheader],format="%Y/%m/%d %H:%M:%S")
-    
+    #readings.timestamp = readings.timestamp.astype("datetime64[s]") - timedelta(hours=1)
+    readings.timestamp = readings.timestamp.astype("datetime64[s]")
+    gps[gheader] = pd.to_datetime(gps[gheader],format=tformat)
+    #remove millis
+    gps[gheader] = gps[gheader].values.astype('datetime64[s]')
+    #remove dublicates
+    gps = gps.drop_duplicates(gheader,keep='last')
     gps_dt = pd.Series(gps[gheader].values, gps[gheader])
     #gps_dt.reindex(readings["timestamp"], method="nearest")
     readings["nearest"] = gps_dt.reindex(readings["timestamp"], method="nearest").values
@@ -97,22 +104,25 @@ def getStarts(source):
     Returns:
         starttime: starttime of readings
         loc: data start location in the file
-
+        deviceid: device id of the sensor
     """
     loc = 0
     with open(source) as f:
-        for line in f:
+        for line in f:            
             if line.startswith('Start at:'):
                 s=line
+            if line.startswith('Operator:'):
+                deviceid = line
             if line.startswith('timestamp'):
                 break
-            loc+=1
+            loc+=1    
     #extract datetime from string
-    #TODO - check for format conversion
-    starttime = pd.to_datetime(str(s.split(":", 1)[1].strip()).replace("-", "").replace("/", "-"))
+    #starttime = pd.to_datetime(str(s.split(":", 1)[1].strip()).replace("-", "").replace("/", "-"))
+    starttime = pd.to_datetime(str(s.split(":", 1)[1].strip()),format="%d/%m/%Y - %H:%M:%S")
+    deviceid = str(deviceid.split(":", 1)[1].strip())
     #convert from nanoseconds
     starttime = starttime.value/10**9 
-    return starttime, loc
+    return starttime, loc, deviceid
 
 def convertTime(data,starttime,human=False):
     """Function for adding start time for readings
@@ -130,28 +140,44 @@ def convertTime(data,starttime,human=False):
         data.timestamp = data.timestamp.astype("datetime64[s]")
     return data
 
+def privacyZone(data,minutes):
+    """Function for cutting off start end of dataset
+
+    Args:
+        data: pandas dataframe object of readings
+        minutes: minutes to cut off
+
+    Returns:
+        data: converted pandas dataframe object of readings
+    """
+    data2 = data.set_index("timestamp")
+    tmin = pd.to_datetime(data2.index.min() + timedelta(minutes=minutes))
+    tmax = pd.to_datetime(data2.index.max() - timedelta(minutes=minutes))
+    data = data[data.timestamp.apply(lambda x: x > tmin) & data.timestamp.apply(lambda x: x < tmax)]
+    return data
+
 def main(argv):
     inputfile = ''
     outputfile = ''
     gpsheader = ''
     gpsfile = ''
     if(len(argv)<1):
-        print 'usage: fidas-parser.py -i <inputfile> [-m <mergeheader>] [-g <gpsfile>] -o <outputfile>'
+        print 'usage: fidas-parser.py -i <inputfile> [-m <mergeformat>] [-g <gpsfile>] -o <outputfile>'
         sys.exit(2);
     try:
-        opts, args = getopt.getopt(argv,"hi:m:g:o:",["ifile=","mhead=","gfile=","ofile="])
+        opts, args = getopt.getopt(argv,"hi:m:g:o:",["ifile=","mform=","gfile=","ofile="])
     except getopt.GetoptError:
-        print 'usage: fidas-parser.py -i <inputfile> [-m <mergeheader>] [-g <gpsfile>] -o <outputfile>'
+        print 'usage: fidas-parser.py -i <inputfile> [-m <mergeformat>] [-g <gpsfile>] -o <outputfile>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print 'fidas-parser.py -i <inputfile> -o <outputfile>'
+            print 'python fidas-parser.py -i <inputfile> [-m <mergeformat>] [-g <gpsfile>] -o <outputfile>'
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
         elif opt in ("-o", "--ofile"):
             outputfile = arg
-        elif opt in ("-m","mhead="):
+        elif opt in ("-m","mform="):
             gpsheader= arg
         elif opt in ("-gps","gpsfile="):
             gpsfile = arg
@@ -161,11 +187,12 @@ def main(argv):
     if gpsheader:
         gps=pd.read_csv(gpsfile,sep=',',header=0)
         data = addGPS(data,gps,gpsheader)
-        #data = addGPS(data,gps,"YYYY-MO-DD HH-MI-SS_SSS")         
-    #add custom header
-    #header = ["timestamp","PM 1","PM 2.5","PM 4","PM 10","PM tot.","dCn","X","Y","time"]
-    #data.to_csv(outputfile,columns = header, index=False)
-    data.to_csv(outputfile,index=False)
+        #data = addGPS(data,gps,"YYYY-MO-DD HH-MI-SS_SSS")      
+    #clean up column names
+    data.columns = data.columns.str.lower().str.replace(":","").str.strip().str.replace(" ", "_")       
+    #custom header
+    #header = ["timestamp","pm_1","pm_2.5","pm_4","pm_10","pm_tot.","dcn", "latitude", "longitude"]
+    data.to_csv(outputfile + deviceid + '.csv',index=False, columns = header, encoding='utf-8',date_format='%Y-%m-%d %H:%M:%S')
     print "Successfully written",outputfile
 
 if __name__ == "__main__":
